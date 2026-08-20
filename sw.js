0a1d9761ce5a9f6112a422b2d526245ac2fd0f0b
codounette1986 <codou.ndiaye@gmail.com>
2026-08-20 10:49:13 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "agripilot-pwa-disabled-v106";
+const cacheName = "agripilot-pwa-disabled-v107";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
